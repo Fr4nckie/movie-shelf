d7e14a40092fa4e35a76bf3fffa84a78e4cbcef3
Franckie <franckie.dev@gmail.com>
2025-08-04 13:24:48 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <h1>Hello world</h1>
+            <button className="btn btn-secondary">Click me</button>
+        </>
+    )
+}
+
+export default App
