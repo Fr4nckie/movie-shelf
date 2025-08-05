78e48c9f94fbeb95597111a8e7a9810abf76b2e1
Franckie <franckie.dev@gmail.com>
2025-08-05 10:05:14 +0300

    feat(router): add Home route to app routing config

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Root from '../pages/Root'
+import Home from '../pages/Home'
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
         children: [
-            { index: true, element: <div>Homepage</div> },
+            { index: true, element: <Home /> },
             { path: 'search', element: <div>Search</div> },
             { path: 'favorites', element: <div>Favorites</div> }
         ]
